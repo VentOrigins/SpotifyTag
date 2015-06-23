9facfc8ddd126c10b31ae7bdca90a2b4d9dd07a2
Randy Truong <rtruo001@ucr.edu>
2015-06-23 16:50:00 -0700

    Added Search Query

--- scripts/search.js
+++ scripts/search.js
@@ -1,10 +1,12 @@
 function search() {
+  //Takes input from the form submit box
   var query = document.getElementById("input-query-box").value;
+  console.log("The query input is: " + query);
 
-  console.log(query);
+  //AJAX Search Tracks of query
   $.ajax({
     type: 'GET',
-    url: "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg",
+    url: "https://api.spotify.com/v1/search?q=" + query + "&type=track",
     dataType: 'json',
     success: function(json) {
       console.log("Here is the response: " + JSON.stringify(json)); 
@@ -14,4 +16,3 @@ function search() {
     }
   });
 } 
-
--- scripts/spotify.js
+++ scripts/spotify.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-  $.ajax({
-      type: 'GET',
-      // beforeSend: function (request) {
-      //   request.setRequestHeader(key, value);
-      // },
-      url: "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg",
-      dataType: 'json',
-      success: function(json) {
-        console.log("Here is the response: " + JSON.stringify(json)); 
-      }
-  });
+  console.log("Document ready");
 });
