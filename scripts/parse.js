75fbf8c03bb53e8ca3d143ec66efc1d7af642fd7
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-06-30 19:25:41 -0700

    update parse but not working

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,111 +1,137 @@
 var hashTagArray = [];
 var tracksArray = [];
-var tracksDB = "";
+var trackDB = "";
 var hashTagDB = "";
+var TrackParse = "";
+var HashTagParse = "";
+
 
 //Test
 $(document).ready(function() {
 	// Our key for parse
 	Parse.initialize("jAKFs2F7wTrMKzSXja7eotc6d2JMC069Nb6z7Qum", "uXJFd72E1JnM88TqJlsLG5Hcx55gdcKQQE7MPRqy");
-	var Tracks = Parse.Object.extend("Tracks");
-	var HashTag = Parse.Object.extend("Hashtags");
-	var query = new Parse.Query(Tracks);
-	//Get tracks db
-	query.get("PwM5gsSoJa", {
-	  success: function(tracks) {
-	    // The object was retrieved successfully.
-	    tracksDB = tracks;
-	  },
-	  error: function(object, error) {
-	    // The object was not retrieved successfully.
-	    // error is a Parse.Error with an error code and message.
-	    console.log("Error in parse tracks" + error);
-	 }
-	});
-	//Get hashtag db
-	query = new Parse.Query(HashTag);
-	query.get("xrzzKKsxdl", {
-	  success: function(hashtags) {
-	    // The object was retrieved successfully.
-	    hashTagDB = hashtags;
-	  },
-	  error: function(object, error) {
-	    // The object was not retrieved successfully.
-	    // error is a Parse.Error with an error code and message.
-	    console.log("Error in parse hashtag " + error);
-	 }
-	});
+	TrackParse = Parse.Object.extend("Tracks");
+	HashTagParse = Parse.Object.extend("Hashtags");
+	trackDB = new Parse.Query(TrackParse);
+	hashTagDB = new Parse.Query(HashTagParse);
 });
 
 //Adds hashtag to track
-function addHTtoTrack(trackName, htValue) {
+function addHTtoTrack(htValue, trackObject) {
 	// return the length of current tracks
-	hashTagArray = tracksDB.get(trackName);
+	hashTagArray = trackObject.get("hashtags");
 	//Check if # exists in array
 	if(hashTagArray.indexOf(htValue) != -1) {
 		return -1;
 	}
 	//Add ht to array
-	tracksDB.add(trackName, htValue);
+	trackObject.add("hashtags", htValue);
 	//Save
-	tracksDB.save();
+	trackObject.save();
 	//Length of array ht so it can be used as id
 	return hashTagArray.length;
 }
 // Checks track's hashtags
 function checkTrackHT(trackName,htValue) {
 
+
+
+
 	//Keep only alphanumeriacl in trackname and value
+	var newTrack = new TrackParse();	
+	var newHT = new HashTagParse();
 	trackName = trackName.replace(/[\W_]+/g, "");
 	htValue = htValue.replace(/[\W_]+/g, "");
-	//Get the tracks object from parse
-	var htExists = tracksDB.get(trackName);
-  //If no tracks then create a new array and add the hashtag to the track
-	if(htExists == undefined) {
-		//Checks to see if the song is in the hashtag db
-		checkHTDB(trackName,htValue);
-  	hashTagArray = [htValue];
-  	tracksDB.set(trackName, hashTagArray);
-  	tracksDB.save();
-		//Return 0 if this was the first track added
-		return 0;
-  }
-	//If there are tracks then use addDBHT function to add the new track
-	else {
-		addHTtoDB(trackName,htValue);
-  	return addHTtoTrack(trackName,htValue);
-	}
+	trackDB.equalTo("tracks", trackName);
+	hashTagDB.equalTo("hashtags", htValue);
+
+	hashTagDB.find({
+	  success: function(results) {
+
+	  	//Check if there is only one of that track
+	  	if(results.length > 1) {
+	    	console.log("Error more than one hashtag object of that hashtag");
+	    }
+	  	//No exisiting track
+	  	else if(results.length == 0) {
+	  		newHT.set("hashtags", htValue);
+	  		newHT.set("tracks",[trackName]);
+	  		newHT.save();
+	  	}
+	    //Add add track to the ht
+	    else {
+	    	addHTtoDB(trackName,results[0]);
+	    }
+	  },
+  	error: function(error) {
+  		//Error
+  		console.log("Error in hashTagDB in checkTrackHT method");
+  	}
+	});
+
+
+
+	trackDB.find({
+	  success: function(results) {
+
+	  	console.log("Test1");
+	    //Check if there is only one of that track
+	  	if(results.length > 1) {
+	    	console.log("Error more than one track object of that track");
+	    }
+	    //Check if track exists
+	  	else if(results.length == 0) {
+	  		//No existing track is found so add new track
+	  		
+				newTrack.set("tracks", trackName);
+				newTrack.set("hashtags", [htValue]);
+				newTrack.save();
+				return 0;
+	  	}
+	    //Add ht to that track
+	    else {
+	    	console.log("Test2");
+	    	return addHTtoTrack(htValue,results[0]);
+	    }
+	  },
+  	error: function(error) {
+  		console.log("Error in trackDB in checkTrackHT");
+  	}
+	});
+	console.log("Test3");
+
 }
 
 //Return all hash tags of track
 function returnTrackHT(trackName) {
 	trackName = trackName.replace(/[\W_]+/g, "");
-	var hashtags = tracksDB.get(trackName);
-	console.log(hashtags);
-	return hashtags;
+	trackDB.equalTo("tracks", trackName);
+	trackDB.find({
+	  success: function(results) {
+	    //Check if there is only one of that track
+	    if(results[0] == undefined) {
+	    	return undefined;
+	    }
+	    if(results.length > 1) {
+	    	alert("Error more than one track object of that track");
+	    }
+	    //Return the hashtag list
+	    else {
+	    	console.log(results[0].get("hashtags"));
+	    	return results[0].get("hashtags");
+	    }
+	  },
+		error: function(error) {
+			console.log("Error in return trackHT");
+		}
+	});
+}
 
-}
-// Check if song exists in given hashtag
-function checkHTDB(trackName, htValue) {
-	var tracksExists = hashTagDB.get(htValue);
-	//Checks if there exists a key with this hashtag
-	
-	//No key so add new key value pair
-	if(tracksExists == undefined) {
-		tracksArray = [trackName];
-		hashTagDB.set(htValue, tracksArray);
-		
-	}
-	//There exists this hashtag key
-	else {
-		addHTtoDB(trackName,htValue);
-	}
-	hashTagDB.save();
-}
 
 //Add trackname to hashtag db
-function addHTtoDB(trackName,htValue) {
-	hashTagDB.add(htValue,trackName);
+function addHTtoDB(trackName,htObject) {
+	// htObject.add("tracks",trackName);
+	// htObject.save();
 }
 
 
